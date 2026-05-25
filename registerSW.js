998f67e3a54b850ddeda9bf88ecde415bc474060
Bruno da MATA <bdamat11@ortizgaming.local>
2026-05-25 13:27:15 -0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/zenjira/sw.js', { scope: '/zenjira/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
